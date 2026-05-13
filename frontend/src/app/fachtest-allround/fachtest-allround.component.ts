42eba7263c5576c091fe3569f41391759077c64c
Kevin <kevin.mart.42@gmail.com>
2026-05-13 22:54:30 +0200

    added fachtest allround 2

--- frontend/src/app/fachtest-allround/fachtest-allround.component.ts
+++ frontend/src/app/fachtest-allround/fachtest-allround.component.ts
@@ -11,6 +11,7 @@ import { FormsModule } from '@angular/forms';
 })
 export class FachtestAllroundComponent {
     input: number | null = null;
+    input2: number | null = null;
 
     get result(): string {
       if (this.input === null) return '—';
@@ -30,7 +31,26 @@ export class FachtestAllroundComponent {
 
     get resultMixed1M2F(): string {
       if (this.input === null) return '—';
-      //((1 * (this.input + 71) / 16.2) + (2 * (this.input + 33) / 16.2)) / (1 + 2)
       return (((2 * (this.input + 71) / 16.2) + (1 * (this.input + 33) / 16.2)) / (2 + 1)).toFixed(2);
+    }
+
+    get result2(): string {
+      if (this.input2 === null) return '—';
+      return ((this.input2 - 46) / 10.6).toFixed(2);
+    }
+
+    get resultMen2(): string {
+      if (this.input2 === null) return '—';
+      return ((this.input2 - 75) / 10.9).toFixed(2);
+    }
+
+    get resultMixed2M1F2(): string {
+      if (this.input2 === null) return '—';
+      return (((1 * (this.input2 - 46) / 10.6) + (2 * (this.input2 - 75) / 10.9)) / (1 + 2)).toFixed(2);
+    }
+
+    get resultMixed1M2F2(): string {
+      if (this.input2 === null) return '—';
+      return (((2 * (this.input2 - 46) / 10.6) + (1 * (this.input2 - 75) / 10.9)) / (2 + 1)).toFixed(2);
     }
   }
